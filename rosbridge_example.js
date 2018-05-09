e867e5089a5878529ac13e76ba56801dc25a1fef
Alymbek Sadybakasov <alymbek.sadybakasov@gmail.com>
2018-05-09 19:47:24 +0200

    beautify html gui

--- rosbridge_example.js
+++ rosbridge_example.js
@@ -82,7 +82,8 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+container = document.getElementById('main');
+renderer.setSize($(container).width(), $(container).height());
 document.getElementById("3d").appendChild(renderer.domElement);
 
 var geometry = new THREE.BoxGeometry(1, 1, 1);
